284498c29b32047eb0e9fd299b689e0cbdf0411c
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-06-17 13:10:14 +0530

    hard coded cycle

--- Leetcode/General/happy_no.cpp
+++ Leetcode/General/happy_no.cpp
@@ -57,3 +57,21 @@ public:
     
     }
 };
+
+// Hardcoded cycle (Any cycle would run into any of these no's only)
+
+def isHappy(self, n: int) -> bool:
+
+    cycle_members = {4, 16, 37, 58, 89, 145, 42, 20}
+
+    def get_next(number):
+        total_sum = 0
+        while number > 0:
+            number, digit = divmod(number, 10)
+            total_sum += digit ** 2
+        return total_sum
+
+    while n != 1 and n not in cycle_members:
+        n = get_next(n)
+
+    return n == 1
