051961e89e0e997e2526e4aecd1b3547f126cfba
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-06-17 14:38:53 +0530

    first unique char in string

---
+++ Leetcode/General/first_unique_ch_str.cpp
@@ -0,0 +1,21 @@
+/*
+Problem link: https://leetcode.com/problems/first-unique-character-in-a-string/
+First unique character in a string
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map <char, int> map; 
+        
+        for (int i=0; i<s.size(); ++i) {  
+            map[s[i]] ++;
+        }
+        
+        for (int i=0; i<s.size(); ++i) {
+            if (map[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
