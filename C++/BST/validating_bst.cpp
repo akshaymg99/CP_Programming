6c561c4c2496534263e266cf0ed40e6030e3b058
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-06-21 16:27:54 +0530

    validating bst iterative approach

--- C++/BST/validating_bst.cpp
+++ C++/BST/validating_bst.cpp
@@ -11,6 +11,8 @@ Validating a Binary Search Tree
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+ // Approach 1: Inrorder check, recursive
  
 class Solution {
 private:
@@ -32,5 +34,33 @@ public:
         }
         prev = root;
         return inorder(root -> right);
+    }
+};
+
+// Approach 2: Using stack for Iterative method
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* prev = nullptr;
+        
+        while ( !stk.empty() || root != nullptr) {
+            while (root != nullptr) {
+                stk.push(root);
+                root = root -> left;
+            }
+            root = stk.top();
+            stk.pop();
+            
+            if (prev != nullptr && root -> val <= prev -> val) 
+                return false;
+            
+            prev = root;
+            root = root -> right;
+            
+        }
+        return true;
+        
     }
 };
