2bd5a91df8f825ccb4d53b889ed6a7c113604d26
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-06-26 13:58:29 +0530

    no of islands

---
+++ Leetcode/General/no_of_islands.cpp
@@ -0,0 +1,54 @@
+/*
+Problem link: https://leetcode.com/problems/number-of-islands
+No of islands 
+*/
+
+// BFS solution
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int nr = grid.size();
+        if (!nr) return 0;
+        int nc = grid[0].size();
+        int num_islands = 0;
+        
+        for (int r=0; r < nr; ++r) {
+            for (int c=0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++ num_islands;  
+                    grid[r][c] = '0';
+                    queue<pair<int, int>> neighbours;
+                    neighbours.push({r, c});
+                    
+                    while(!neighbours.empty()) {
+                        auto rc = neighbours.front();
+                        neighbours.pop();
+                        int row = rc.first, col = rc.second;
+                        if (row-1 >=0 && grid[row-1][col] == '1') {
+                            neighbours.push({row-1, col});
+                            grid[row-1][col] = '0';
+                        }
+                            
+                        if (row+1 < nr && grid[row+1][col] == '1') {
+                            neighbours.push({row+1, col});
+                            grid[row+1][col] = '0';
+                        }
+                            
+                        if (col - 1 >=0 && grid[row][col-1] == '1') {
+                            neighbours.push({row, col-1});
+                            grid[row][col-1] = '0';
+                        }
+                            
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbours.push({row, col+1});
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return num_islands;
+        
+    }
+};
