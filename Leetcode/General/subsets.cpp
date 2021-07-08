ffc83e1df354fc68206f259753f23b7cdf1fb6c3
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-07-08 21:06:57 +0530

    subsets by bactracking

--- Leetcode/General/subsets.cpp
+++ Leetcode/General/subsets.cpp
@@ -25,3 +25,31 @@ public:
         return ans;
     }
 };
+
+// Backtracking Approach
+
+class Solution {
+private:
+    vector< vector<int>> ans;
+    
+    void sub(vector<int>&temp, vector<int>&nums, int start) {
+        if (start == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        
+        temp.push_back(nums[start]);
+        sub(temp, nums, start+1);
+        temp.pop_back();
+        sub(temp, nums, start+1);
+    }
+    
+    
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        sub(temp, nums, 0);
+        return ans;
+        
+    }
+};
