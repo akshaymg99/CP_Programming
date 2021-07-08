232cdbf712ddf54f384ccd51265f65d2bf5133f5
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-07-08 23:40:08 +0530

    kth largest using STL

--- Leetcode/General/kth_largest_arr.cpp
+++ Leetcode/General/kth_largest_arr.cpp
@@ -15,3 +15,11 @@ public:
        return pq.top();
     }
 };
+
+// STL nth element
+
+int findKthLargest(vector<int>& nums, int k) {
+	nth_element(nums.begin(), nums.begin()+nums.size()-k, nums.end());
+	return nums[nums.size()-k];
+}
+
