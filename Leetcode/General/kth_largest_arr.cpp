ca8e2501b9e649792c3ce2dc83a49a1ac0183eae
Akshay Gudiyawar <akshaymg99@gmail.com>
2021-07-09 23:30:40 +0530

    updated kth largest in arr

--- Leetcode/General/kth_largest_arr.cpp
+++ Leetcode/General/kth_largest_arr.cpp
@@ -3,6 +3,8 @@ Problem link: https://leetcode.com/problems/kth-largest-element-in-an-array
 Kth largest element in an array
 */
 
+// Priority queue as MAX HEAP
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -23,3 +25,20 @@ int findKthLargest(vector<int>& nums, int k) {
 	return nums[nums.size()-k];
 }
 
+// With Partial sort
+
+int findKthLargest(vector<int>& nums, int k) {
+	partial_sort(nums.begin(), nums.begin()+nums.size()-k+1, nums.end());
+	return nums[nums.size()-k];
+}
+
+// Max Heap using multiset
+
+int findKthLargest(vector<int>& nums, int k) {
+	multiset<int, greater<int>> mst(nums.begin(),nums.end());
+
+	for(int i=1; i<k; ++i) 
+		mst.erase(mst.begin());
+	
+	return *mst.begin();
+}
